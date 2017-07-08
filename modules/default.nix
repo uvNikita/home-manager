b98f08ae4e1f43783188f43ff25247dd8318a14c
Robert Helgesson <robert@rycee.net>
2017-07-08 21:52:24 +0200

    xmonad: add service
    When enabled this will set the X session window manager to xmonad.

--- modules/default.nix
+++ modules/default.nix
@@ -37,6 +37,7 @@ let
     ./services/taffybar.nix
     ./services/tahoe-lafs.nix
     ./services/udiskie.nix
+    ./services/xmonad.nix
     ./services/xscreensaver.nix
     ./systemd.nix
     ./xresources.nix
